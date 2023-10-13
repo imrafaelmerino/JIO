a2ca7a17bd2088ef21bd50ab0b1434f7abf12c1d
Rafael Merino García <imrafaelmerino@gmail.com>
2023-10-13 12:45:50 +0200

    readme

--- jio-http/src/main/java/jio/http/client/oauth/RefreshTokenLoop.java
+++ jio-http/src/main/java/jio/http/client/oauth/RefreshTokenLoop.java
@@ -1,30 +1,23 @@
 package jio.http.client.oauth;
 
 
-import jio.Lambda;
-import jio.http.client.MyHttpClient;
-
-import java.util.function.Predicate;
-
 /**
- * This exception happens when the predicate specified in
- * {@link ClientCredentialsHttpClientBuilder#ClientCredentialsHttpClientBuilder(MyHttpClient, Lambda, Lambda,
- * Predicate)} to check when refreshing the token is evaluated to true for a predefined number of times in a row,
- * producing the following loop:
+ * This exception happens when the predicate to check when refreshing the oauth token, that is specified in
+ * {@link ClientCredentialsHttpClientBuilder}, is evaluated to true for a predefined number of times in a row, producing
+ * the following loop:
  *
  * <pre>
  *
- *     GET token
- *     http request
+ *     GET token http request
  *     refreshToken = true
  *
- *     GET token
- *     http request
+ *     GET token http request
  *     refreshToken = true
  *
- *     GET token
- *     http request
+ *     GET token http request
  *     refreshToken = true
+ *
+ *     AND so on
  *
  *
  *
@@ -39,15 +32,15 @@ import java.util.function.Predicate;
  * In this scenario you'll get this exception if the BACKEND returns the status code 401. The problem is that the
  * refreshTokenPredicates only checks the status code and cant distinguish a regular 401 from the backend from a 401
  * from the API gateway asking you to refresh the token. You need to be more specific and also take into account a
- * header or a body
+ * header or the body
  */
 @SuppressWarnings("serial")
 public final class RefreshTokenLoop extends Exception {
 
     RefreshTokenLoop(int n) {
         super("The refresh token predicate has been evaluated to true for " + n + " times in a row. It could" +
-                      "be an error on its implementation because it's returning true to ask for a new access token" +
-                      "when it shouldn't.");
+              "be an error on its implementation because it's returning true to ask for a new access token" +
+              "when it shouldn't.");
     }
 }
 
